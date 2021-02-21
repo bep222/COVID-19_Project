3ba15769393f597d624e62e57aa0d23a60ee2a58
Beatrice Pinetti <bep222@lehigh.edu>
2021-02-21 22:42:49 +0100

    Assignment 2

--- Assignment2.R
+++ Assignment2.R
@@ -1,4 +1,19 @@
 Temp <- getwd()
+
 setwd("./covid-19-data/")
+
 USStates <- read.csv("us-states.csv")
+
 setwd(Temp)
+
+Pennsylvania <- filter(USStates, "state"=="Pennyslvania")
+
+Pennsylvania$adj_deaths <- 0
+
+adj_deaths <- ("adj_deaths"-filter("state","New York""Kansas City, Mo.""Joplin, Mo.""Alaska""Alameda County, Calif.""Douglas County, Neb.""Chicago""Guam""Puerto Rico")
+
+If(Pennsylvania$date=="21-04-2020") [Pennsylvania$adj_deaths <- (Pennsylvania$adj_deaths -282)]
+
+If(Pennsylvania$date=="22-04-2020") [Pennsylvania$adj_deaths <- (Pennsylvania$adj_deaths -297)]
+
+SUM(Pennsylvania$adj_deaths)
